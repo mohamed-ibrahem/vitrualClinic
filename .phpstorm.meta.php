f7ba18332f134103b93adfff66cbb672d41cb46d
Mohamed Ibrahim <mohamed.ibr94@gmail.com>
2018-11-08 13:53:38 +0200

    Update .env

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2018-11-07 17:03:44.
+    * Generated on 2018-11-08 12:15:27.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
