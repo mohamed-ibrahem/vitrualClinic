0fb8356f9f93e394b53a78386d05fa394046883b
mohamed-ibrahim <mohamed.ibr1994@gmail.com>
2019-01-28 10:38:47 +0200

    Ready For Notifications

--- config/app.php
+++ config/app.php
@@ -147,6 +147,8 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
+        NotificationChannels\FCM\ServiceProvider::class,
+
         /*
          * Application Service Providers...
          */
